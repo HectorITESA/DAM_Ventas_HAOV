d49131428e1f810d6cdb1439e2932dd28e859d56
Hector <Hector@DESKTOP-M6SHBEU>
2025-03-07 15:11:18 -0600

    Versión_02: Se creo la BD con EFC

---
+++ Ventas.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ventas.Share.Entities;
+
+namespace Ventas.API.Data
+{
+    public class DataContext : DbContext
+
+    {
+        public DataContext(DbContextOptions<DataContext> optios) : base(optios)
+        {
+            
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
